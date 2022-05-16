b397556b1377daed0096b997bdb4ddca9e6a9c63
Karly-22 <karly.holloway@hotmail.com>
2022-05-16 15:04:56 +0100

    remove console.log

--- models/categories.js
+++ models/categories.js
@@ -3,7 +3,6 @@ const db = require('../db/connection');
 exports.fetchCategories = () => {
     return db.query('SELECT * FROM categories;')
         .then((result) => {
-            console.log(result.rows)
             return result.rows;
         })
 };
