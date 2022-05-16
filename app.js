9d430f6cca03fcc8feb8e9ceb10f992d64dbe2b0
Karly-22 <karly.holloway@hotmail.com>
2022-05-16 15:02:38 +0100

    add 404 error and 500 server error

--- app.js
+++ app.js
@@ -7,5 +7,13 @@ const {
 
 app.get('/api/categories', getCategories);
 
+app.all('/api/*',(req, res) => {
+    res.status(404).send({ msg: 'Route not found'})
+})
+
+app.use((err, req, res, next) => {
+    console.log(err);
+    res.sendStatus(500);
+  });
 
 module.exports = app;
