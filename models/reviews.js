eae955fa455ef37fe7cefcb756313f40a3fffe3b
Karly-22 <karly.holloway@hotmail.com>
2022-05-17 17:06:46 +0100

    add custom 400 and 404 errors to updateVote

--- models/reviews.js
+++ models/reviews.js
@@ -11,11 +11,17 @@ exports.fetchSingleReview = (review_id) => {
 };
 
 exports.updateVote = (review_id, inc_votes) => {
-
+    if(!Number.isInteger(inc_votes)) {
+        return Promise.reject({ status: 400, msg: 'Bad request: invalid input' });
+    }
         return db.query('UPDATE reviews SET votes = votes + $1 WHERE review_id = $2 RETURNING *;', [inc_votes, review_id])
             .then((result) => {
+                if(!result.rows.length) {
+                    console.log('hi')
+                    return Promise.reject({ status: 404, msg: 'Route not found' })
+                };
                 return result.rows[0];
-            })
+            });
     
 };
 
