17faa9f0b6afbf1c057431c3fca5108a1698bf86
Karly-22 <karly.holloway@hotmail.com>
2022-05-18 10:09:52 +0100

    change err message on updateVote and fetchSingleReview

--- models/reviews.js
+++ models/reviews.js
@@ -4,21 +4,18 @@ exports.fetchSingleReview = (review_id) => {
     return db.query(`SELECT * FROM reviews WHERE review_id = $1`, [review_id])
     .then((result) => {
         if (!result.rows.length) {
-            return Promise.reject({ status: 404, msg: 'Route not found' })
+            return Promise.reject({ status: 404, msg: `No review found for review_id: ${review_id}` })
         };
         return result.rows[0];
     });
 };
 
 exports.updateVote = (review_id, inc_votes) => {
-    if(!Number.isInteger(inc_votes)) {
-        return Promise.reject({ status: 400, msg: 'Bad request: invalid input' });
-    }
         return db.query('UPDATE reviews SET votes = votes + $1 WHERE review_id = $2 RETURNING *;', [inc_votes, review_id])
             .then((result) => {
                 if(!result.rows.length) {
                     console.log('hi')
-                    return Promise.reject({ status: 404, msg: 'Route not found' })
+                    return Promise.reject({ status: 404, msg: `No review found for review_id: ${review_id}` })
                 };
                 return result.rows[0];
             });
