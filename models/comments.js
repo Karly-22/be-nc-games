ed219a0c146b12ae626139c9c5e1c63a0bdebac7
Karly-22 <karly.holloway@hotmail.com>
2022-05-19 12:56:49 +0100

    add second query to check for existence of review and refactor commentBody query

--- models/comments.js
+++ models/comments.js
@@ -1,20 +1,14 @@
 const db = require('../db/connection');
 
 exports.fetchReviewComments = (review_id) => {
-    return db.query(`
-        SELECT
-            comment_id,
-            votes,
-            created_at,
-            author,
-            body,
-            review_id 
-        FROM comments WHERE review_id = $1`, [review_id])
-        .then((result) => {
-            console.log(result.rows)
-            if(!result.rows.length) {
-                return Promise.reject({ status: 404, msg: `No review found for review_id: ${review_id}`})
-            }
-            return result.rows;
-        });
+    const reviewBody = db.query(`SELECT review_id FROM reviews WHERE review_id = $1;`, [review_id]);
+    const commentBody = db.query(`SELECT * FROM comments WHERE review_id = $1`, [review_id])
+
+        return Promise.all([reviewBody, commentBody])
+            .then(([reviewBody, commentBody]) => {
+                if(!reviewBody.rows.length) {
+                    return Promise.reject({ status: 404, msg: `No review found for review_id: ${review_id}`});
+                } 
+                return commentBody.rows;
+            });
 };
