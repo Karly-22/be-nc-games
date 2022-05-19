a4fb00ddb75e165c1a43c00ef30e47f89f5536d0
Karly-22 <karly.holloway@hotmail.com>
2022-05-19 11:22:09 +0100

    add 200 responds with correct result test for /api/reviews/:review_id/comments endpoint

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -222,3 +222,27 @@ describe('GET /api/reviews', () => {
             }); 
     });
 });
+
+describe('GET /api/reviews/:review_id/comments', () => {
+    test('200: should respond with an array of comments for given review_id', () => {
+        return request(app)
+            .get('/api/reviews/2/comments')
+            .expect(200)
+            .then(({ body: {comments}}) => {
+                expect(comments).toBeInstanceOf(Array);
+                expect(comments).toHaveLength(3);
+                    comments.forEach((comment) => {
+                        expect(comment).toEqual(
+                            expect.objectContaining({
+                                comment_id: expect.any(Number),
+                                votes: expect.any(Number),
+                                created_at: expect.any(String),
+                                author: expect.any(String),
+                                body: expect.any(String),
+                                review_id: expect.any(Number)
+                            })
+                        )
+                    })
+            })
+    });
+});
