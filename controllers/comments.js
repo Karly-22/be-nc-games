9493887bfabfeb795cd66cee85eb4feb69a34e33
Karly-22 <karly.holloway@hotmail.com>
2022-05-19 11:35:59 +0100

    add to getReviewComments and fetchReviewComments to handel 400 and 404 errors

--- controllers/comments.js
+++ controllers/comments.js
@@ -4,5 +4,7 @@ exports.getReviewComments = (req, res, next) => {
     const { review_id } = req.params;
     fetchReviewComments(review_id).then((comments) => {
         res.status(200).send({ comments });
-    });
+    }).catch((err) => {
+        next(err);
+    })
 };
--- models/comments.js
+++ models/comments.js
@@ -1,7 +1,6 @@
 const db = require('../db/connection');
 
 exports.fetchReviewComments = (review_id) => {
-    console.log(review_id)
     return db.query(`
         SELECT
             comment_id,
@@ -12,6 +11,10 @@ exports.fetchReviewComments = (review_id) => {
             review_id 
         FROM comments WHERE review_id = $1`, [review_id])
         .then((result) => {
+            console.log(result.rows)
+            if(!result.rows.length) {
+                return Promise.reject({ status: 404, msg: `No review found for review_id: ${review_id}`})
+            }
             return result.rows;
         });
 };
