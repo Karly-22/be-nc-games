b7fe099efe01e727fe91204a14d1d318d018c494
Karly-22 <karly.holloway@hotmail.com>
2022-05-20 09:56:19 +0100

    add error handing for post request

--- controllers/errors.js
+++ controllers/errors.js
@@ -7,8 +7,18 @@ exports.handleCustomErrors = (err, req, res, next) => {
 exports.handlePSQLErrors = (err, req, res, next) => {
     if (err.code === '22P02') {
         res.status(400).send({ msg: 'Bad request'})
-    } else next (err);
+    } else next(err);
 };
+
+exports.handleInsertReviewCommentError = (err, req, res, next) => {
+    if(err.code === '23503') {
+        res.status(404).send({ msg: 'Route not found'})
+    } else if (err.code === '23502') {
+        res.status(400).send({ msg: 'No username or message input'})
+    } else {
+        next(err);
+    }
+}
 
 exports.handleInternalServerError = (err, req, res, next) => {
     console.log(err);
