be9e24370a79c4a0766e890bbd021bb78f1964a5
Karly-22 <karly.holloway@hotmail.com>
2022-05-20 12:03:25 +0100

    add sort_by query to fetchReviews

--- models/reviews.js
+++ models/reviews.js
@@ -27,8 +27,8 @@ exports.updateVote = (review_id, inc_votes) => {
     
 };
 
-exports.fetchReviews = () => {
-    return db.query(`
+exports.fetchReviews = (sort_by = 'created_at') => {
+    let queryStr = `
     SELECT 
         reviews.review_id,    
         reviews.owner, 
@@ -40,9 +40,15 @@ exports.fetchReviews = () => {
     COUNT(comments.review_id)::INT AS comment_count 
     FROM reviews
     LEFT JOIN comments ON comments.review_id = reviews.review_id 
-    GROUP BY reviews.review_id
-    ORDER BY created_at DESC;`)
-        .then((result) => {
+    GROUP BY reviews.review_id`
+    
+    if(!['title', 'created_at', 'votes'].includes(sort_by)) {
+        return Promise.reject({status: 400, msg: 'Bad request'})
+    }
+
+    queryStr += ` ORDER BY ${sort_by} DESC;`
+
+    return db.query(queryStr).then((result) => {
             return result.rows;
         });
 };
